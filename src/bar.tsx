f46632d20a6bcbd38e58e264860e5229e840a329
a-wing <1@233.email>
2023-10-19 01:06:16 +0800

    add bar camera

--- src/bar.tsx
+++ src/bar.tsx
@@ -1,14 +1,34 @@
-import React, { useRef } from 'react'
+import { useState } from 'react'
 import WHIPClient from './lib/whip'
 import { useAtom } from 'jotai'
 import { meAtom } from './atom'
 
+
+const deviceScreen = {
+  deviceId: "screen",
+  groupId: "",
+  kind: "videoinput",
+  label: "screen",
+  toJSON: () => { },
+}
+
 export default function App() {
-  //const [video, setVideo] = useRef<HTMLVideoElement>()
   const [me] = useAtom(meAtom)
+  const [permission, setPermission] = useState("unknow")
+  const [currentDeviceAudio, setCurrentDeviceAudio] = useState()
+  const [currentDeviceVideo, setCurrentDeviceVideo] = useState()
+  const [deviceAudio, setDeviceAudio] = useState<MediaDeviceInfo[]>([])
+  const [deviceVideo, setDeviceVideo] = useState<MediaDeviceInfo[]>([])
 
   const start = async () => {
-    const stream = await navigator.mediaDevices.getDisplayMedia({ audio: false, video: true })
+    const deviceId = currentDeviceVideo
+    let stream
+    if (deviceId === "screen") {
+      stream = await navigator.mediaDevices.getDisplayMedia({ audio: false, video: true })
+    } else {
+      stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    }
+
     const pc = new RTCPeerConnection();
     pc.addTransceiver(stream.getVideoTracks()[0], {
       direction: 'sendonly',
@@ -25,10 +45,49 @@ export default function App() {
     whip.publish(pc, url, token);
   }
 
+  const refresh = async () => {
+    navigator.getUserMedia({
+      video: true,
+      audio: true
+    }, () => setPermission("success"), () => setPermission("error"))
+
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    const audios = []
+    const videos = []
+    for (const device of devices) {
+      switch (device.kind) {
+        case 'videoinput':
+          videos.push(device)
+          break;
+
+        case 'audioinput':
+          audios.push(device)
+          break;
+      }
+    }
+
+    setDeviceAudio(audios)
+    setDeviceVideo([...videos, deviceScreen])
+  }
+
   return (
     <div className='flex justify-evenly' bg="green-400 hover:green-500 ">
       <label>Bar</label>
-      <button onClick={() => { start() }}>start video</button>
+      <button onClick={() => { start() }}>start</button>
+
+      <div>
+        <label>{permission}</label>
+        <button onClick={() => { refresh() }}>refresh</button>
+        <label>Video Device:</label>
+        <select
+          value={currentDeviceVideo}
+          onChange={e => setDeviceVideo(e.target.value)}
+        >
+          {deviceVideo.map(device =>
+            <option key={device.deviceId} value={device.deviceId}>{device.label}</option>
+          )}
+        </select>
+      </div>
     </div>
   )
 }
