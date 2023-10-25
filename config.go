b528d1d8f2cd046ade4a19244e7f2e49a11c24b7
a-wing <1@233.email>
2023-10-25 15:29:24 +0800

    add fly.io deploy

--- config.go
+++ config.go
@@ -2,6 +2,6 @@ package main
 
 type config struct {
 	Port        string `env:"PORT" envDefault:"4000"`
-	DatabaseUrl string `env:"DATABASEI_URL" envDefault:"user=postgres password=password dbname=woom sslmode=disable"`
+	DatabaseUrl string `env:"DATABASE_URL" envDefault:"user=postgres password=password dbname=woom sslmode=disable"`
 	Live777Url  string `env:"LIVE777_URL" envDefault:"http://localhost:3000"`
 }
