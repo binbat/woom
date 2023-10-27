6bb5d00da60648a781c8fbbceac3a1a83edeaaae
a-wing <1@233.email>
2023-10-27 22:29:31 +0800

    refactor(server): http file server

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strconv"
-	"strings"
 	"woom/database"
 
 	_ "github.com/lib/pq"
@@ -33,25 +32,6 @@ func handler(p http.Handler, token string) func(http.ResponseWriter, *http.Reque
 		}
 		p.ServeHTTP(w, r)
 	}
-}
-
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
 }
 
 func main() {
@@ -82,9 +62,6 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	//r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	//	w.Write([]byte("welcome"))
-	//})
 
 	r.Post("/room/", func(w http.ResponseWriter, r *http.Request) {
 		uuid := r.URL.Query().Get("uuid")
@@ -99,7 +76,6 @@ func main() {
 
 		var roomId int
 		db.QueryRow(`INSERT INTO rooms (stream) VALUES ($1) RETURNING id;`, h).Scan(&roomId)
-		// fmt.Println(room)
 		w.Write([]byte(strconv.Itoa(roomId)))
 	})
 
@@ -117,14 +93,8 @@ func main() {
 			Id     int
 			Stream hstore.Hstore
 		}
-		//room := Room{
-		//	Id:     0,
-		//	Stream: make(map[string]sql.NullString),
-		//}
 		var room Room
 		db.QueryRow(`SELECT id, stream FROM rooms WHERE id = $1;`, roomId).Scan(&room.Id, &room.Stream)
-		// db.QueryRow(`SELECT id, stream FROM rooms WHERE id = $1;`, roomId).Scan(&room.Id, stream)
-		// db.QueryRow(`SELECT stream FROM rooms WHERE id = $1;`, roomId).Scan(&stream)
 		log.Println(room)
 
 		var rooms []string
@@ -143,8 +113,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// FileServer(r, "/", http.FS(fsys))
-	FileServer(r, "/", NewSPA("index.html", http.FS(fsys)))
+	r.Handle("/*", http.StripPrefix("/", http.FileServer(NewSPA("index.html", http.FS(fsys)))))
 
 	log.Println("=== started ===")
 	log.Panicln(http.ListenAndServe(":"+cfg.Port, r))
