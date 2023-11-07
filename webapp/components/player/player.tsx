4ce3d212924393142fb737e31a88fdb2ccee2c3e
a-wing <1@233.email>
2023-11-07 17:51:47 +0800

    feat(webapp): player wavesurfer

--- webapp/components/player/player.tsx
+++ webapp/components/player/player.tsx
@@ -1,8 +1,30 @@
 import { useEffect, useRef } from 'react'
 import { UserStream } from '../../store/atom'
+import WaveSurfer from 'wavesurfer.js'
+import RecordPlugin from 'wavesurfer.js/dist/plugins/record'
 
 export default function Player(props: { user: UserStream, muted: boolean }) {
   const refVideo = useRef<HTMLVideoElement>(null)
+  const refWave = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (refWave.current && props.user.stream) {
+      const wavesurfer = WaveSurfer.create({
+        container: refWave.current,
+        waveColor: 'rgb(200, 100, 0)',
+        progressColor: 'rgb(100, 50, 0)',
+      })
+
+      const record = wavesurfer.registerPlugin(RecordPlugin.create())
+      const { onDestroy, onEnd } = record.renderMicStream(props.user.stream)
+
+      return () => {
+        onDestroy()
+        onEnd()
+        wavesurfer.destroy()
+      }
+    }
+  }, [refWave.current, props.user.stream])
 
   useEffect(() => {
     if (refVideo.current) {
@@ -16,6 +38,7 @@ export default function Player(props: { user: UserStream, muted: boolean }) {
         ? <video className='rounded-xl' autoPlay={true} controls={false} muted={props.muted} style={{ width: '640px' }} ref={refVideo} />
         : null
       }
+      <div className='rounded-xl' ref={refWave}></div>
       <center className='text-white'>
         {props.user.name}
       </center>
