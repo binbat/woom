7564671142a01c3b534e029a1a4e4867300d6c87
a-wing <1@233.email>
2023-11-20 02:14:11 +0800

    refactor(webapp): prepare use player

--- webapp/components/prepare.tsx
+++ webapp/components/prepare.tsx
@@ -1,34 +1,27 @@
-import { useRef, useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useAtom } from 'jotai'
 import DeviceBar from './device'
 import Loading from "./loading"
+import Player from './player/player'
 import {
   displayNameAtom,
   localStreamIdAtom,
   meetingJoinedAtom,
   localStreamAtom,
-  currentDeviceAudioAtom,
   localUserStatusAtom,
 } from '../store/atom'
 
 import { asyncGetStreamId } from '../lib/storage'
 
-import WaveSurfer from 'wavesurfer.js'
-import RecordPlugin from 'wavesurfer.js/dist/plugins/record'
-
 export default function Prepare(props: { meetingId: string }) {
   const [loading, setLoading] = useState<boolean>(false)
-  const refWave = useRef<HTMLDivElement>(null)
 
-  const refVideo = useRef<HTMLVideoElement>(null)
   const [displayName, setDisplayName] = useAtom(displayNameAtom)
 
+  const [localStream] = useAtom(localStreamAtom)
   const [localStreamId, setLocalStreamId] = useAtom(localStreamIdAtom)
   const [localUserStatus, setLocalUserStatus] = useAtom(localUserStatusAtom)
   const [_, setMeetingJoined] = useAtom(meetingJoinedAtom)
-
-  const [localStream, setLocalStream] = useAtom(localStreamAtom)
-  const [currentDeviceAudio, setCurrentDeviceAudio] = useAtom(currentDeviceAudioAtom)
 
   const start = async () => {
     setLoading(true)
@@ -47,41 +40,10 @@ export default function Prepare(props: { meetingId: string }) {
     setMeetingJoined(true)
   }
 
-  const onChangeVideo = async () => {
-    if (refVideo.current) {
-      refVideo.current.srcObject = localStream.stream
-    }
-  }
-
-  useEffect(() => {
-    onChangeVideo()
-  }, [localStream])
-
-  useEffect(() => {
-    if (!!refWave.current) {
-      const wavesurfer = WaveSurfer.create({
-        container: refWave.current,
-        waveColor: 'rgb(200, 100, 0)',
-        progressColor: 'rgb(100, 50, 0)',
-      })
-
-      if (currentDeviceAudio !== "none") {
-        const record = wavesurfer.registerPlugin(RecordPlugin.create())
-        record.startRecording({ deviceId: currentDeviceAudio })
-      }
-
-      return () => {
-        wavesurfer.destroy()
-      }
-    }
-  }, [currentDeviceAudio])
-
   return (
     <div className='flex flex-col justify-around'>
       <center className='m-xs'>
-        <video className='rounded-xl' playsInline={true} autoPlay={true} controls={false} ref={refVideo} style={{ width: '320px' }}></video>
-
-        <div className='rounded-xl' ref={refWave}></div>
+        <Player user={localStream} muted={false} />
       </center>
 
       <center className='mb-xs'>
