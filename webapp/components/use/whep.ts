4deebd20ed3dfbae732830a9da3ce837fcc3a063
a-wing <1@233.email>
2024-01-16 18:01:34 +0800

    refactor(webapp): use whep

--- webapp/components/use/whep.ts
+++ webapp/components/use/whep.ts
@@ -22,80 +22,94 @@ class Context extends EventTarget {
     this.sync()
   }
 
+  cache: any
+
+  constructor(id: string) {
+    super()
+    this.id = id
+    this.clone()
+  }
+
+  clone() {
+    this.cache = {
+      id: this.id,
+      stream: this.stream,
+      userStatus: this.userStatus,
+      start: () => this.start(),
+      restart:  () => this.restart(),
+    }
+  }
+
+  export = () => this.cache
+
+  private newPeerConnection() {
+    const { pc, setStream } = this
+    pc.addTransceiver('video', { 'direction': 'recvonly' })
+    pc.addTransceiver('audio', { 'direction': 'recvonly' })
+    pc.ontrack = ev => setStream(ev.streams[0])
+  }
+
   syncUserStatus = (_: UserStatus) => {}
   setSyncUserStatus = (callback: (userStatus: UserStatus) => void) => {
     callback(this.userStatus)
     this.syncUserStatus = callback
   }
 
-  sync = () => {
-    ctx = Object.assign({}, this)
+  sync() {
+    this.clone()
     this.dispatchEvent(event)
   }
-  start = start
-  restart = restart
-}
+  async start() {
+    const { id, pc, client, userStatus } = this
+    pc.onconnectionstatechange = () => {
+      userStatus.state = pc.connectionState
+      this.sync()
+      this.syncUserStatus(userStatus)
+    }
+    userStatus.state = 'signaled'
+    this.sync()
+    this.syncUserStatus(userStatus)
+    this.newPeerConnection()
 
-function newPeerConnection() {
-  const { pc, setStream } = getContext()
-  pc.addTransceiver('video', { 'direction': 'recvonly' })
-  pc.addTransceiver('audio', { 'direction': 'recvonly' })
-
-  pc.ontrack = ev => setStream(ev.streams[0])
-}
-
-const context = new Context()
-let ctx = Object.assign({}, context)
-
-function getContext() {
-  return ctx
-}
-
-async function start() {
-  const { id, pc, client, userStatus, sync, syncUserStatus } = context
-  pc.onconnectionstatechange = () => {
-    userStatus.state = pc.connectionState
-    sync()
-    syncUserStatus(userStatus)
+    try {
+      const url = location.origin + `/whep/${id}`
+      await client.view(pc, url)
+    } catch (e) {
+      console.log(e)
+      userStatus.state = 'failed'
+      this.syncUserStatus(userStatus)
+      this.sync()
+    }
   }
-  userStatus.state = 'signaled'
-  sync()
-  syncUserStatus(userStatus)
-  newPeerConnection()
-
-  try {
-    const url = location.origin + `/whep/${id}`
-    await client.view(pc, url)
-  } catch (e) {
-    console.log(e)
-    userStatus.state = 'failed'
-    syncUserStatus(userStatus)
-    sync()
+  async restart() {
+    await this.client.stop()
+    this.pc = new RTCPeerConnection()
+    await this.start()
+    this.sync()
   }
 }
 
-async function restart() {
-  await context.client.stop()
-  context.pc = new RTCPeerConnection()
-  start()
-}
-
+const contexts: Context[] = []
 function run() {
   console.log("=== RUN ===")
-  const { userStatus } = context
-  if (userStatus.state === "") start()
+  //const { userStatus } = context
+  //if (userStatus.state === "") start()
 
   //if (userStatus.state !== "connected" && userStatus.state !== "signaled") restart()
 }
 
-function subscribe(callback: () => void) {
-  context.addEventListener(event.type, callback)
-  return () => {
-    context.removeEventListener(event.type, callback)
+export default function useWhipClient(id: string) {
+  const newContext = (id: string) => {
+    const context = new Context(id)
+    contexts.push(context)
+    return context
   }
-}
 
-export default function useWhipClient(streamId: string) {
-  context.id = streamId
-  return useSyncExternalStore(subscribe, getContext)
+  const context = contexts.find(ctx => ctx.id === id) || newContext(id)
+  return useSyncExternalStore((callback: () => void) => {
+    context.addEventListener(event.type, callback)
+    return () => {
+      context.removeEventListener(event.type, callback)
+    }
+  }, () => context.export())
 }
